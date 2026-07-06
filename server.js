36ad0ca3012f86a6faec87eae150484fbe72ca49
toolrfid1-crypto <toolrfid1@gmail.com>
2026-07-06 16:11:37 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require("express");
+const path = require("path");
 const fs = require("fs");
 
 const app = express();
 
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/data.json", (req, res) => {
+    res.sendFile(path.join(__dirname, "data.json"));
+});
 
 app.post("/submit", (req, res) => {
+
+    console.log("ได้รับข้อมูล:", req.body);
 
     const file = "data.json";
 
@@ -26,22 +37,6 @@ app.post("/submit", (req, res) => {
 
 });
 
-app.get("/data", (req, res) => {
-
-    console.log("GET /data");
-
-    const file = "data.json";
-
-    if (!fs.existsSync(file)) {
-        return res.json([]);
-    }
-
-    const json = JSON.parse(fs.readFileSync(file, "utf8"));
-
-    res.json(json);
-
-});
-
 app.listen(3000, () => {
-    console.log("Server started");
+    console.log("Server started on http://localhost:3000");
 });
